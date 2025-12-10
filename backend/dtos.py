d4ab69308ec39234f007dbb2325497393caa3c54
twaal <theodor.walberg@gmail.com>
2025-12-10 21:05:59 +0100

    delete 'backend' the NMiKI challenge

--- backend/dtos.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class TumorPredictRequestDto(BaseModel):
-    image: str  # base64 encoded image
-
-class TumorPredictResponseDto(BaseModel):
-    segmentation: str  # base64 encoded mask
-    success: bool
-    message: str = ""
