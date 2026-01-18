2450de241a66657dbaecd4f49bf4bf326ab00078
twaal <theodor.walberg@gmail.com>
2026-01-18 23:37:21 +0100

    open sesame

--- cell-counter-yolov4/log/README.md
+++
@@ -1,5 +0,0 @@
-# Training logs
-
-Open the logs in this directory with:
-
-tensorboard --logdir=log
--- cell-counter-yolov4/utils/ImageRescaler.py
+++
@@ -1,7 +0,0 @@
-"""A function for rescaling images to 1024x768 pixels using opencv."""
-
-import cv2
-
-
-def rescale_im(img):
-    return cv2.resize(img, dsize=(1024, 768), interpolation=cv2.INTER_CUBIC)
